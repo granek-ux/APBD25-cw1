e7521ec2aa16b300a555d87e5647a742b24d8052
Jakub Baliński <s31418@pjwstk.edu.pl>
2025-03-04 16:50:33 +0100

    ver 4

--- Cwiczenia1/Cwiczenia1/Program.cs
+++ Cwiczenia1/Cwiczenia1/Program.cs
@@ -1,6 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!!!!");
-Console.WriteLine("Hello, World!!!!");
-Console.WriteLine("Hello, World!!!!");
-Console.WriteLine("Hello, World!!!!");
-Console.WriteLine("Hello, World!!!!");
+Console.WriteLine("Hello, World!");
+
+
+
+
+static double GetAverage(int[] arr)
+{
+    double sum = 0;
+
+    for (int i = 0; i<arr.Length; i++)
+    {
+        sum += arr[i];
+    }
+    return sum/arr.Length;
+}
+
+int[] arr = { 1, 2, 3, 4, 5 };
+Console.WriteLine(
+GetAverage(arr));
